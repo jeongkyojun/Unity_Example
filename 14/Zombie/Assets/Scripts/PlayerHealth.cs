081bfac0a75bc2e1d2ca2a37e733910d1266b067
jeongkyojun <kyojun2002@naver.com>
2020-10-28 23:04:06 +0900

    zombie survival

--- 14/Zombie/Assets/Scripts/PlayerHealth.cs
+++ 14/Zombie/Assets/Scripts/PlayerHealth.cs
@@ -17,23 +17,53 @@ public class PlayerHealth : LivingEntity {
 
     private void Awake() {
         // 사용할 컴포넌트를 가져오기
+        playerAnimator = GetComponent<Animator>();
+        playerAudioPlayer = GetComponent<AudioSource>();
+
+        playerMovement = GetComponent<PlayerMovement>();
+        playerShooter = GetComponent<PlayerShooter>();
     }
 
     protected override void OnEnable() {
         // LivingEntity의 OnEnable() 실행 (상태 초기화)
         base.OnEnable();
+        
+        // 체력 슬라이더 활성화
+        healthSlider.gameObject.SetActive(true);
+
+        // 체력 슬라이더의 최댓값을 기본 체력값으로 변경
+        healthSlider.maxValue = startingHealth;
+
+        // 체력 슬라이더의 값을 현재 체력값으로 변경
+        healthSlider.value = health;
+
+        //플레이어 조작을 받는 컴포넌트 활성화
+        playerMovement.enabled = true;
+        playerShooter.enabled = true;
     }
 
     // 체력 회복
     public override void RestoreHealth(float newHealth) {
         // LivingEntity의 RestoreHealth() 실행 (체력 증가)
         base.RestoreHealth(newHealth);
+
+        //갱신된 체력으로 체력슬라이더 갱신
+        healthSlider.value = health;
     }
 
     // 데미지 처리
     public override void OnDamage(float damage, Vector3 hitPoint, Vector3 hitDirection) {
         // LivingEntity의 OnDamage() 실행(데미지 적용)
+        if(!dead)
+        {
+            //사망하지 않은 경우만 효과음 재생
+            playerAudioPlayer.PlayOneShot(hitClip);
+        }
+
+        // LivingEntity의 OnDamage() 실행(데미지 적용)
         base.OnDamage(damage, hitPoint, hitDirection);
+        // 갱신된 체력을 체력 슬라이더에 반영
+        healthSlider.value = health;
     }
 
     // 사망 처리
