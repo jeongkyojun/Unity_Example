cab97c1350046626cfee8792e8294dd2444bd7cf
jeongkyojun <kyojun2002@naver.com>
2020-12-12 20:03:29 +0900

    how to move camera

--- MovePractice_2D_sideView/Assets/Scripts/Playermove.cs
+++ MovePractice_2D_sideView/Assets/Scripts/Playermove.cs
@@ -20,12 +20,23 @@ public class Playermove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.x + playerInput.rotate * Time.deltaTime * speed<8.5 && transform.position.x + playerInput.rotate * Time.deltaTime * speed > -8.5)
+        if(transform.position.x + playerInput.rotate * Time.deltaTime * speed<100 && transform.position.x + playerInput.rotate * Time.deltaTime * speed > -100)
             transform.position = new Vector2(transform.position.x + playerInput.rotate * Time.deltaTime * speed, transform.position.y);
-        
+        else
+        {
+            set_return();
+        }
         if(Input.GetKey(KeyCode.Space))
         {
             rigidbody2D.AddForce(new Vector2(0, 3));
         }
+    }
+
+    void set_return()
+    {
+        if (transform.position.x + playerInput.rotate * Time.deltaTime * speed < 100)
+            transform.position = new Vector2(99, transform.position.y);
+        else if (transform.position.x + playerInput.rotate * Time.deltaTime * speed > -100)
+            transform.position = new Vector2(-99, transform.position.y);
     }
 }
