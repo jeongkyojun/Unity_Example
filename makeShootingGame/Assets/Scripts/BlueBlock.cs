af2c76555944b5b9a109ccc2d6951a2db4727291
jeongkyojun <kyojun2002@naver.com>
2020-12-23 21:27:57 +0900

    make rhythm Platform

---
+++ makeShootingGame/Assets/Scripts/BlueBlock.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueBlock : MonoBehaviour
+{
+    //3초 존재, 1초 비존재
+    private float PastTime;
+    private bool isActive;
+    private float x, y, z;
+    private float ActiveNum = 3f, MaxNum = 4f,cnt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cnt = 0;
+        MaxNum = 4;
+        ActiveNum = 2;
+        PastTime = 0f;
+        isActive = true;
+        x = transform.position.x;
+        y = transform.position.y;
+        z = transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - PastTime >= 1)
+        {
+            PastTime = Time.time;
+            if (isActive)
+            {
+                transform.position = new Vector3(x, y, z);
+                isActive = false;
+            }
+            else if(cnt > ActiveNum/MaxNum)
+            {
+                transform.position = new Vector3(x, y, 21);
+                isActive = true;
+            }
+            if (++cnt == MaxNum)
+                cnt = 0;
+        }
+    }
+}
---
+++ makeShootingGame/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private int cnt = 0;
+    private float pastTime=0;
+    private float speed = 5f;
+    PlayerInput playerInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        cnt = 0;
+        pastTime = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(playerInput.rotate * Time.deltaTime*speed, playerInput.move * Time.deltaTime*speed, 0));
+    }
+}
