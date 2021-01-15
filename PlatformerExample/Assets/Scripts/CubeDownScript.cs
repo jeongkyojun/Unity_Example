344696fc715cfec00d7c94e1dbc7a2cf2ea6df5e
jeongkyojun <kyojun2002@naver.com>
2021-01-15 20:24:26 +0900

    Unity platformer

---
+++ PlatformerExample/Assets/Scripts/CubeDownScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeDownScript : MonoBehaviour
+{
+    Vector3 firstPosition;
+    bool isDown;
+    float speed = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        firstPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isDown)
+            Move(firstPosition);
+    }
+
+    void Move(Vector3 firstPosition)
+    {
+        if (transform.position.y < firstPosition.y)
+            transform.position += Vector3.up * speed * Time.deltaTime;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.collider.tag == "Player")
+        {
+            Debug.Log(other.contacts[0].normal.y);
+            if(other.contacts[0].normal.y < -0.7f)
+            {
+                isDown = true;
+            }
+        }
+    }
+
+    void OnCollisionStay(Collision other)
+    {
+        if(isDown && other.collider.tag == "Player")
+        {
+            transform.position -= Vector3.up * speed * Time.deltaTime;
+        }
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        isDown = false;
+        
+    }
+}
---
+++ PlatformerExample/Assets/Scripts/MonoEyeScript.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonoEyeScript : MonoBehaviour
+{
+    public GameObject eyes;
+    Vector3 firstPosition;
+    Vector3 playerPosition;
+
+    float attackTime;
+    bool find_player = false;
+
+    bool isActive = false;
+    float direction = -1f;
+
+    float speed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        firstPosition = eyes.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isActive)
+        {
+            Move(playerPosition);
+            if (find_player)
+            {
+                if (Time.time - attackTime > 3)
+                    Attack();
+            }
+        }
+        else
+        {
+            Move(firstPosition);
+        }
+    }
+
+    void Move(Vector3 playerPosition)
+    {
+        if (playerPosition.x < eyes.transform.position.x)
+            direction = -1;
+        else if (playerPosition.x > eyes.transform.position.x)
+            direction = 1;
+        else
+            direction = 0;
+
+        eyes.transform.position += Vector3.right * direction * speed * Time.deltaTime;
+        //Debug.Log(eyes.transform.position.x);
+        if (!find_player)
+        {
+            if ((direction > 0 && eyes.transform.position.x > playerPosition.x) || (direction < 0 && eyes.transform.position.x < playerPosition.x))
+            {
+                Debug.Log("Find");
+                attackTime = Time.time;
+                find_player = true;
+            }
+        }
+    }
+
+    void Attack()
+    {
+        attackTime = Time.time;
+        Debug.Log("Attack!");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            isActive = true;
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+            playerPosition = other.gameObject.transform.position;
+    }
+
+    void OnTriggerExit()
+    {
+        isActive = false;
+    }
+}
