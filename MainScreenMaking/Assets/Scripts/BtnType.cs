4837356cbfa896060593c724aab54b98fb7b4cad
jeongkyojun <kyojun2002@naver.com>
2021-02-03 16:35:26 +0900

    MainScreenDemo

---
+++ MainScreenMaking/Assets/Scripts/BtnType.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BtnType : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler
+{
+    public ButtonType currentType;
+    //public Transform buttonScale;
+    Vector3 defaultScale;
+    public CanvasGroup mainGroup;
+    public CanvasGroup optionGroup;
+    
+    bool isSound = true;
+
+    private void Start()
+    {
+        //defaultScale = buttonScale.localScale;
+        defaultScale = transform.localScale;
+    }
+    public void OnBtnClick()
+    {
+        switch (currentType)
+        {
+            case ButtonType.New:
+                Debug.Log("새게임");
+                ScreenLoader.LoadSceneHandle("Play", 0);
+                break;
+            case ButtonType.Continue:
+                Debug.Log("이어하기");
+                ScreenLoader.LoadSceneHandle("Play", 1);
+                break;
+            case ButtonType.Option:
+                Debug.Log("설정");
+                CanvasGroupOn(optionGroup);
+                CanvasGroupOff(mainGroup);
+                break;
+            case ButtonType.Sound:
+                Debug.Log("소리");
+                if (isSound)
+                {
+                    Debug.Log("사운드 off");
+                }
+                else
+                {
+                    Debug.Log("사운드 on");
+                }
+                isSound = !isSound;
+                break;
+            case ButtonType.Back:
+                Debug.Log("뒤로가기");
+                CanvasGroupOn(mainGroup);
+                CanvasGroupOff(optionGroup);
+                break;
+            case ButtonType.Quit:
+                Debug.Log("끝내기");
+#if UNITYENGINE
+                UnityEditor.EditorApplication.isPlaying = false;
+#else
+                Application.Quit();
+#endif
+                break;
+        }
+    }
+
+    public void CanvasGroupOn(CanvasGroup cg)
+    {
+        cg.alpha = 1;
+        cg.interactable = true;
+        cg.blocksRaycasts = true;
+    }
+    public void CanvasGroupOff(CanvasGroup cg)
+    {
+        cg.alpha = 0;
+        cg.interactable = false;
+        cg.blocksRaycasts = false;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        //buttonScale.localScale = defaultScale * 1.2f;
+        transform.localScale = defaultScale * 1.2f;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        //buttonScale.localScale = defaultScale;
+        transform.localScale = defaultScale;
+    }
+}
---
+++ MainScreenMaking/Assets/Scripts/ScreenLoader.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScreenLoader : MonoBehaviour
+{
+    public Slider progressbar;
+    public Text loadtext;
+    public static string loadScene;
+    public static int loadType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadScene());
+    }
+
+    public static void LoadSceneHandle(string _name, int _loadType)
+    {
+        loadScene = _name;
+        loadType = _loadType;
+        SceneManager.LoadScene("Loading");
+    }    
+
+    IEnumerator LoadScene()
+    {
+        yield return null;
+        AsyncOperation operation = SceneManager.LoadSceneAsync(loadScene);
+        operation.allowSceneActivation = false;
+
+        while(!operation.isDone)
+        {
+            yield return null;
+            if (loadType == 0)
+            {
+                Debug.Log("새게임");
+                /*
+                 * 모든 데이터를 초기값으로 적용하고 시작한다.
+                 */
+            }
+            else if (loadType == 1)
+            {
+                Debug.Log("불러오기");
+                /*
+                 * 저장된 데이터의 값을 불러온 뒤 시작한다.
+                 */
+            }
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
